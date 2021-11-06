d353fb512e75d6c4c11312565f8c8b8d1ded5c33
Hemory Phifer <hphife@yahoo.com>
2021-11-05 22:41:10 -0400

    solution

---
+++ main.py
@@ -0,0 +1,44 @@
+adjective1 = "bluish"
+noun1 = "house"
+past_tense_verb1 = "ran"
+adverb1 = "frantically"
+adjective2 = "pretty"
+noun2 = "toe"
+noun3 = "baseball"
+adjective3 = "amazing"
+verb = "drive"
+past_tense_verb2 = "saw"
+adjective4 = "grumpy"
+adverb2 = "loudly"
+
+
+print(f"Today I went to the zoo. I saw a {adjective1} {noun1} jumping up and down in its tree.")
+print(f"He {past_tense_verb1} {adverb1} through the large tunnel that led to its {adjective2} {noun2}.")
+print(f"I got some peanuts and passed them through the cage to a gigantic gray {noun3} towering above my head.")
+print(f"Feeding that animal made me hungry. I went to get a {adjective3} scoop of icecream.")
+print(f"It filled my stomach.")
+print(f"Afterwards I had to {verb} {adverb2} to catch our bus.")
+print(f"When I got home I {past_tense_verb2} my mom for a {adjective4} day at the zoo.")
+
+# challenge
+
+adjective1 = input("Enter adjective: ")
+noun1 = input("Enter noun: ")
+past_tense_verb1 = input("Enter past tense verb: ")
+adverb1 = input("Enter adverb: ")
+adjective2 = input("Enter adjective: ")
+noun2 = input("Enter noun: ")
+noun3 = input("Enter noun: ")
+adjective3 = input("Enter adjective: ")
+verb = input("Enter verb: ")
+past_tense_verb2 = input("Enter past tense verb: ")
+adjective4 = input("Enter adjective: ")
+adverb2 = input("Enter adverb: ")
+
+print(f"Today I went to the zoo. I saw a {adjective1} {noun1} jumping up and down in its tree.")
+print(f"He {past_tense_verb1} {adverb1} through the large tunnel that led to its {adjective2} {noun2}.")
+print(f"I got some peanuts and passed them through the cage to a gigantic gray {noun3} towering above my head.")
+print(f"Feeding that animal made me hungry. I went to get a {adjective3} scoop of icecream.")
+print(f"It filled my stomach.")
+print(f"Afterwards I had to {verb} {adverb2} to catch our bus.")
+print(f"When I got home I {past_tense_verb2} my mom for a {adjective4} day at the zoo.")
